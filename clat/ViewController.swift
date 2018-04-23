39148b366f42d2f72f70db0096fccda1d9bc178f
Kai Yan <loliwarder@kais-mac-mini.local>
2018-04-23 10:03:22 -0700

    Initial Commit

---
+++ clat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  clat
+//
+//  Created by Kai Yan on 4/23/18.
+//  Copyright © 2018 Kai Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
