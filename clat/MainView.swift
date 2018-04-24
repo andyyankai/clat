bb10be014d00244966b99934f14039d6206dfcae
Kai Yan <loliwarder@kais-mac-mini.local>
2018-04-24 09:12:35 -0700

    MainView

---
+++ clat/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  clat
+//
+//  Created by Kai Yan on 4/23/18.
+//  Copyright © 2018 Kai Yan. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
